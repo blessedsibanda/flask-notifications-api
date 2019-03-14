1411bc2090affe0ea64ffbd00bc609f56dc84ff4
Blessed Sibanda <n01519451g@students.nust.ac.zw>
2019-03-14 14:03:56 +0200

    Delete config.py

--- service/config.py
+++
@@ -1,8 +0,0 @@
-import os 
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_ECHO = False
-SQLALCHEMY_TRACK_MODIFICATIONS = True 
-
-SQLALCHEMY_DATABASE_URI = "postgresql://{DB_USER}:{DB_PASS}@{DB_ADDR}/{DB_NAME}".format(DB_USER="notificationsuser", DB_PASS="1234", DB_ADDR="127.0.0.1", DB_NAME="notificationsdb") 
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
